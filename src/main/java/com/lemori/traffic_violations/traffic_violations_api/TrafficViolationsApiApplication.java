89f71e9a62ec9c75a6effac568e27d9314e85f4e
g.lemos <gabriel_lemos06@hotmail.com>
2025-01-12 20:43:48 -0300

    initial: initiating spring project

---
+++ src/main/java/com/lemori/traffic_violations/traffic_violations_api/TrafficViolationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lemori.traffic_violations.traffic_violations_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficViolationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficViolationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemori/traffic_violations/traffic_violations_api/TrafficViolationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemori.traffic_violations.traffic_violations_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficViolationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
